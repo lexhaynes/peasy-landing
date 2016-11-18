3882cba5328907c672fa5236a88b6a6e143e8316
Alex Haynes <alex@Alex.local>
2016-11-18 12:07:55 -0500

    added coming soon page; added social icons to footer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('hello');
+var nodes = {
+	nav: document.getElementsByClassName('global-header')[0],
+	banner: document.getElementsByClassName('banner')[0],
+}
+
+var els = {
+	navClientHeight: Number(window.getComputedStyle(nodes.nav).height.replace(/\D/g,'')),
+	bannerClientHeight: Number(window.getComputedStyle(nodes.banner).height.replace(/\D/g,'')),
+}
+
+var handlers = {
+	scroll: function() {
+		if (window.scrollY > els.bannerClientHeight) {
+			if (!nodes.nav.classList.contains('inverted')) nodes.nav.classList.add('inverted');
+		} else if (nodes.nav.classList.contains('inverted')) nodes.nav.classList.remove('inverted');
+	}
+}
+
+window.addEventListener('scroll', handlers.scroll, false);
